b24cbb474a60194f654a2dcbd1926cc866de2bff
Ananya Phadke <phadkeananya@gmail.com>
2018-07-11 21:21:46 +0000

    automatically backed up by learn

--- weather.rb
+++ weather.rb
@@ -1,7 +1,7 @@
 # Your methods should take in a hash as an argument - here's an example of what the hash will look like - the methods should take this hash as an argument and return the desired output. 
 
 weather = {
-:location => "New York",
+:city => "New York",
 :description => "Partly Cloudy",
 :temperature_farenheit => 66.3
 }
@@ -9,7 +9,7 @@ weather = {
 # 1. Complete the method below called location, that accepts a hash called weather as an argument so that it returns the value of city from the weather hash. 
 
 def location(weather)
-	weather[:location]
+	weather[:city]
 end
 
 # 2. Using the location method as a guide, write a  method called "description" so it returns the description from the weather hash. 
@@ -21,7 +21,7 @@ end
 # 3. Using the methods above and string interpolation, update the method below so that it returns a string that describes the current weather - for example: "The weather in New York is Partly Cloudy. The temperature is 66.1 degrees Farenheit." HINT: the method should RETURN the string, not print it to the terminal. 
 
 def weather_report(weather)
-  "The weather in #{weather[:location]} is #{weather[:description]}. The temperature is #{weather[:temperature_farenheit]} degrees Farenheit"
+  "The weather in #{weather[:city]} is #{weather[:description]}. The temperature is #{weather[:temperature_farenheit]} degrees Farenheit"
 end
 
 #4. Write a method that takes in the weather hash as an argument. 
